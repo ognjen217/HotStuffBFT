2b05f293deff5b0f5ac2b9fdf6020a2af73b02d7
Ognjen Peric <92032437+ognjen217@users.noreply.github.com>
2026-06-22 20:31:18 +0200

    Add Basic HotStuff banking simulator tests

---
+++ internal/hotstuff/safenode_test.go
@@ -0,0 +1,41 @@
+package hotstuff
+
+import "testing"
+
+type testCommand string
+
+func (c testCommand) ID() string     { return string(c) }
+func (c testCommand) String() string { return string(c) }
+
+func TestSafeNodeAcceptsExtensionOfLockedNode(t *testing.T) {
+	genesis := NewGenesisNode()
+	lockedNode := NewNode(genesis, testCommand("a"), "B1", 1)
+	child := NewNode(lockedNode, testCommand("b"), "B2", 2)
+	lockedQC := &QC{Phase: PhasePreCommit, View: 1, NodeID: lockedNode.ID, Quorum: 3, Voters: []string{"B1", "B2", "B3"}, Votes: []Vote{{"B1", PhasePreCommit, 1, lockedNode.ID}, {"B2", PhasePreCommit, 1, lockedNode.ID}, {"B3", PhasePreCommit, 1, lockedNode.ID}}}
+	justify := &QC{Phase: PhasePrepare, View: 1, NodeID: lockedNode.ID, Quorum: 3, Voters: []string{"B1", "B2", "B3"}, Votes: []Vote{{"B1", PhasePrepare, 1, lockedNode.ID}, {"B2", PhasePrepare, 1, lockedNode.ID}, {"B3", PhasePrepare, 1, lockedNode.ID}}}
+	if !SafeNode(child, justify, lockedQC) {
+		t.Fatal("expected extension of locked node to be safe")
+	}
+}
+
+func TestSafeNodeRejectsConflictWithStaleQC(t *testing.T) {
+	genesis := NewGenesisNode()
+	lockedNode := NewNode(genesis, testCommand("a"), "B1", 1)
+	conflict := NewNode(genesis, testCommand("x"), "B2", 2)
+	lockedQC := &QC{Phase: PhasePreCommit, View: 3, NodeID: lockedNode.ID, Quorum: 3, Voters: []string{"B1", "B2", "B3"}, Votes: []Vote{{"B1", PhasePreCommit, 3, lockedNode.ID}, {"B2", PhasePreCommit, 3, lockedNode.ID}, {"B3", PhasePreCommit, 3, lockedNode.ID}}}
+	justify := &QC{Phase: PhasePrepare, View: 2, NodeID: GenesisID, Quorum: 3, Voters: []string{"B1", "B2", "B3"}, Votes: []Vote{{"B1", PhasePrepare, 2, GenesisID}, {"B2", PhasePrepare, 2, GenesisID}, {"B3", PhasePrepare, 2, GenesisID}}}
+	if SafeNode(conflict, justify, lockedQC) {
+		t.Fatal("expected conflicting node with stale QC to be rejected")
+	}
+}
+
+func TestSafeNodeAcceptsConflictWithHigherQC(t *testing.T) {
+	genesis := NewGenesisNode()
+	lockedNode := NewNode(genesis, testCommand("a"), "B1", 1)
+	conflict := NewNode(genesis, testCommand("x"), "B2", 4)
+	lockedQC := &QC{Phase: PhasePreCommit, View: 3, NodeID: lockedNode.ID, Quorum: 3, Voters: []string{"B1", "B2", "B3"}, Votes: []Vote{{"B1", PhasePreCommit, 3, lockedNode.ID}, {"B2", PhasePreCommit, 3, lockedNode.ID}, {"B3", PhasePreCommit, 3, lockedNode.ID}}}
+	justify := &QC{Phase: PhasePrepare, View: 4, NodeID: GenesisID, Quorum: 3, Voters: []string{"B1", "B2", "B3"}, Votes: []Vote{{"B1", PhasePrepare, 4, GenesisID}, {"B2", PhasePrepare, 4, GenesisID}, {"B3", PhasePrepare, 4, GenesisID}}}
+	if !SafeNode(conflict, justify, lockedQC) {
+		t.Fatal("expected conflicting node with higher QC to be accepted")
+	}
+}
